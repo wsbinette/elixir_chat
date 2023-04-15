875fdb8e712cf15d4afbfe3cc6800e886be2f395
Will Binette <wsbinette@icloud.com>
2023-04-15 15:09:08 -0400

    assign room name

--- lib/chat_app_web/controllers/chat_controller.ex
+++ lib/chat_app_web/controllers/chat_controller.ex
@@ -2,6 +2,12 @@ defmodule ChatAppWeb.ChatController do
   use ChatAppWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index)
+    titleRaw = Mnemonic.generate()
+    |> String.split()
+    |> Enum.take(2)
+    |> Enum.map_join(" ", &(String.capitalize(&1)))
+    
+    page_title = "#{titleRaw} Room"
+    render(conn, :index, page_title: page_title)
   end
 end
