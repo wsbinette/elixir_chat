6175d06f85402bdcbf6b840f083d6234d374d7cf
Will Binette <wsbinette@icloud.com>
2023-04-16 01:32:27 -0400

    message lists

--- lib/chat_app_web/controllers/chat_controller.ex
+++ lib/chat_app_web/controllers/chat_controller.ex
@@ -1,13 +1,17 @@
 defmodule ChatAppWeb.ChatController do
   use ChatAppWeb, :controller
 
+  alias ChatApp.{Message}
+
   def index(conn, _params) do
     titleRaw = Mnemonic.generate()
     |> String.split()
     |> Enum.take(2)
     |> Enum.map_join(" ", &(String.capitalize(&1)))
-    
+
+    message_list = Message.list_messages()
+
     page_title = "#{titleRaw} Room"
-    render(conn, :index, page_title: page_title)
+    render(conn, :index, page_title: page_title, message_list: message_list)
   end
 end
