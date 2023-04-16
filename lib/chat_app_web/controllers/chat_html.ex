e9dd8e32b2339bdbebeb6c6fa80fac062dc8f046
Will Binette <wsbinette@icloud.com>
2023-04-16 01:32:17 -0400

    make chat box controller

--- lib/chat_app_web/controllers/chat_html.ex
+++ lib/chat_app_web/controllers/chat_html.ex
@@ -2,9 +2,29 @@ defmodule ChatAppWeb.ChatHTML do
   use ChatAppWeb, :html
 
 
-  def index(assigns) do
+  embed_templates "chat_html/*"
+
+  attr :user_name, :string
+  attr :message, :string
+  attr :message_time, :string
+
+  def chat_box(assigns) do
     ~H"""
-    Hello!
+      <div class="mt-5 border-2 border-orange-500 h-1/5 px-5 rounded-lg shadow-md">
+          <div class="w-full pt-2 flex items-center justify-between">
+              <h3 class="font-bold text-orange-500">
+                <%= @user_name %>
+              </h3>
+              <p class="text-gray-500 font-medium text-sm">
+                <%= @message_time %>
+              </p>
+          </div>
+          <div class="p-2">
+              <p class="text-gray-500">
+                <%= @message %>
+              </p>
+          </div>
+      </div>
     """
   end
 end
